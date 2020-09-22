e1d64cb9b735ca93b422fba3238a743af1a1b4ea
ym_ci-g <56593656+ess4536@users.noreply.github.com>
2020-09-22 16:19:38 +0900

    6.3.2 completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200918120704) do
+ActiveRecord::Schema.define(version: 20200922064804) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

bf5769810f8e3677daf68cb1691e01360cf4b0ce
ym_ci-g <56593656+ess4536@users.noreply.github.com>
2020-09-24 13:42:36 +0900

    7.4 completed

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,21 +5,20 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
-  test "invalid signup information" do
+  test "valid signup information" do
     get signup_path
-    assert_no_difference 'User.count' do
+    assert_difference 'User.count' do
       post users_path, params: { 
         user: {
-          name: "",
-          email: "user@invalid",
-          password: "foo",
-          password_confirmation: "bar"
+          name: "Example User",
+          email: "user@example.com",
+          password: "password",
+          password_confirmation: "password"
         }
       }
     end
-    assert_template 'users/new'
-    assert_select 'div#error_explanation'
-    assert_select 'div.field_with_errors'
-    assert_select 'form[action="/signup"]'
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.blank?
   end
 end

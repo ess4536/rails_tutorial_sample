675e5c355c5a6abaff0e13827a1967a57541d5c3
ym_ci-g <56593656+ess4536@users.noreply.github.com>
2020-09-24 10:56:22 +0900

    7.4.1 completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存成功時
+      redirect_to @user
     else
       render 'new'
     end

d7b3ece2eecd5e342b4f870c8268bc80398d0541
ym_ci-g <56593656+ess4536@users.noreply.github.com>
2020-10-20 21:05:06 +0900

    8.2.1 completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # user created
+      log_in user
+      redirect_to user
     else
       # error
       flash.now[:danger] = 'Invalid email/password combination'

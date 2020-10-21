b80e7292ff45d29b1aca53663d9a5cad0826b2a2
ym_ci-g <56593656+ess4536@users.noreply.github.com>
2020-10-21 14:45:39 +0900

    8.2.4 completed

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -4,6 +4,9 @@ class UserLoginTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = users(:michael)
+  end
 
   test "login with invalid information" do
     get login_path
@@ -13,5 +16,18 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session:
+                                {email: @user.email,
+                                  password: 'password'} }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
